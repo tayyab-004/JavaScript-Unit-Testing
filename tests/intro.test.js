23114975706de6a4a34b3bff7daec91546d993f1
tayyab-004 <tayyabgee333@gmail.com>
2024-07-23 23:51:17 +0500

    Factorial Test

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { max, fizzBuzz, calculateAverage } from "../src/intro";
+import { max, fizzBuzz, calculateAverage, factorial } from "../src/intro";
 
 describe("max", () => {
     it("should return first argument as greater", ()=> {
@@ -48,5 +48,31 @@ describe("calculateAverage", () => {
 
     it("should calculate the average of an array with three elements", ()=> {
         expect(calculateAverage([1, 2, 3])).toBe(2);
+    });
+})
+
+describe ("factorial", () => {
+    it ("should return 1 if given 0", () => {
+        expect(factorial(0)).toBe(1);
+    });
+
+    it ("should return 1 if given 1", () => {
+        expect(factorial(1)).toBe(1);
+    });
+
+    it ("should return 2 if given 2", () => {
+        expect(factorial(2)).toBe(2);
+    });
+
+    it ("should return 6 if given 3", () => {
+        expect(factorial(3)).toBe(6);
+    });
+
+    it ("should return 24 if given 4", () => {
+        expect(factorial(4)).toBe(24);
+    });
+
+    it ("should return undefined if given number is negative", () => {
+        expect(factorial(-1)).toBeUndefined();
     });
 })
