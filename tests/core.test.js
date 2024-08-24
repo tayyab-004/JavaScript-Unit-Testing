8bac8bd4444359afd78e7542377124dd61417c35
tayyab-004 <tayyabgee333@gmail.com>
2024-08-24 18:34:53 +0500

    test for array of objects

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,10 +1,18 @@
 import { it, expect, describe } from 'vitest'
+import { getCoupons } from '../src/core';
 
-describe('test suite', () => {
-    it('test case', () => {
-        const result = { name: "Tayyab", id: 1 };
-        expect(result).toMatchObject({ name: "Tayyab" });
-        expect(result).toHaveProperty( "name" );
-        expect(typeof result.name).toBe("string");
+describe('getCoupons', () => {
+    it('should return an array of coupons', () => {
+        const coupons = getCoupons();
+        expect(Array.isArray(coupons)).toBe(true);
+        expect(coupons.length).toBeGreaterThan(0);
+    })
+    it('should return an array with valid coupon codes', () => {
+        const coupon =getCoupons();
+        coupon.forEach(coupon => {
+            expect(coupon).toHaveProperty('code');
+            expect(typeof coupon.code).toBe('string');
+            expect(coupon.code).toBeTruthy();
+        });
     })
 })
