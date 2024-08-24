676911e9b0844031146115dc8ef9316fc49d3f7e
tayyab-004 <tayyabgee333@gmail.com>
2024-08-24 18:11:02 +0500

    assertion test

--- tests/core.test.js
+++ tests/core.test.js
@@ -2,7 +2,9 @@ import { it, expect, describe } from 'vitest'
 
 describe('test suite', () => {
     it('test case', () => {
-        const result = { name: "Tayyab" };
-        expect(result).toEqual({ name: "Tayyab" });
+        const result = { name: "Tayyab", id: 1 };
+        expect(result).toMatchObject({ name: "Tayyab" });
+        expect(result).toHaveProperty( "name" );
+        expect(typeof result.name).toBe("string");
     })
 })
