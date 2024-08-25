8b4015b453eec90e9ee0fcbbfc8120bfe558f4d7
tayyab-004 <tayyabgee333@gmail.com>
2024-08-25 12:28:38 +0500

    Asynchronous Testing

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,5 +1,5 @@
 import { it, expect, describe } from 'vitest'
-import { calculateDiscount, canDrive, getCoupons, isPriceInRange, isValidUsername, validateUserInput } from '../src/core';
+import { calculateDiscount, canDrive, fetchData, getCoupons, isPriceInRange, isValidUsername, validateUserInput } from '../src/core';
 
 describe('getCoupons', () => {
     it('should return an array of coupons', () => {
@@ -144,5 +144,16 @@ describe('canDrive', () => {
         {age: 18, country: 'UK', result: true}
     ])(`should return $result for $age, $country`, ({age, country, result}) => {
     expect(canDrive(age, country)).toBe(result);
+    })
+})
+
+describe('fetchData', () => {
+    it('should return a promise that will resolve to an array of numbers', async() => {
+        try {
+            const result = await fetchData();
+        } catch (error) {
+            expect(error).toHaveProperty('reason');
+            expect(error.reason).toMatch(/fail/i);
+        }
     })
 })
