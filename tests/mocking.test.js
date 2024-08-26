6b2e695655fce89e0f9330e236b029e1bacb936b
tayyab-004 <tayyabgee333@gmail.com>
2024-08-26 15:22:47 +0500

    Mocking Module

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,4 +1,8 @@
 import { vi, it, expect, describe } from 'vitest'
+import { getPriceInCurrency } from '../src/mocking';
+import { getExchangeRate } from '../src/libs/currency';
+
+vi.mock('../src/libs/currency');
 
 describe('Mock', () => {
     it('sendText ok', () => {
@@ -14,5 +18,14 @@ describe('Mock', () => {
 
         // assertion for that result is 'ok'
         expect(result).toBe('ok');
+    })
+})
+
+describe('getPriceInCurrency', () => {
+    it('should return price in target currency', () => {
+        vi.mocked(getExchangeRate).mockReturnValue(1.5)
+        const price = getPriceInCurrency(10, 'PKR');
+
+        expect(price).toBe(15);
     })
 })
