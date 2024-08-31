da9adc28450ce36e3fdf0bac704944c915b3c699
tayyab-004 <tayyabgee333@gmail.com>
2024-08-31 16:47:46 +0500

    working of spying with functions

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,10 +1,11 @@
 import { vi, it, expect, describe } from 'vitest'
-import { getPriceInCurrency, getShippingInfo, renderPage, signUp, submitOrder } from '../src/mocking';
+import { getPriceInCurrency, getShippingInfo, login, renderPage, signUp, submitOrder } from '../src/mocking';
 import { getExchangeRate } from '../src/libs/currency';
 import { getShippingQuote } from '../src/libs/shipping';
 import { trackPageView } from '../src/libs/analytics';
 import { charge } from '../src/libs/payment';
 import { sendEmail } from '../src/libs/email';
+import security from '../src/libs/security';
 
 vi.mock('../src/libs/currency');
 vi.mock('../src/libs/shipping');
@@ -125,5 +126,18 @@ describe('signUp', () => {
         const args = vi.mocked(sendEmail).mock.calls[0];
         expect(args[0]).toBe(email);
         expect(args[1]).toMatch(/welcome/i);
+    })
+})
+
+describe('login', () => {
+    const email = 'tayyabgee333@gmail.com';
+
+    it('should email one-time login code', async() => {
+        const spy = vi.spyOn(security, 'generateCode');
+
+        await login(email);
+
+        const securityCode = spy.mock.results[0].value.toString();
+        expect(sendEmail).toHaveBeenCalledWith(email, securityCode)
     })
 })
